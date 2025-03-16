4d2ee594a4d6bec9ab6c9d688454d7a3dd585f70
JoeNutty <amirhusni2001@gmail.com>
2025-03-16 08:25:41 +0800

    comapny setup

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        \App\Models\User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
